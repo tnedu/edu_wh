f726e7ebea47ff137006ede3b3da8db846a37ad8
Steven McKee <steven.mckee@tnedu.gov>
2025-11-06 16:23:05 -0600

    Fix because databricks is acting weird about variants suddenly.

--- models/build/edfi_3/bld_ef3__tenant_lea_ownership.sql
+++ models/build/edfi_3/bld_ef3__tenant_lea_ownership.sql
@@ -4,7 +4,8 @@
 -- keep the definition from the tenant that actually has ownership
 -- since the relationship between tenant and district is not codified anywhere,
 -- we observe it from student enrollments.
-select distinct ssa.tenant_code, lea_id
+select distinct ssa.tenant_code, 
+    cast(lea_id as int) as lea_id
 from {{ ref('stg_ef3__student_school_associations') }} ssa 
 join {{ ref('stg_ef3__schools') }} sch 
     on ssa.k_school = sch.k_school
